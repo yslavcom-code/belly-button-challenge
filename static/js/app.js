9fed1691e3a5cd0c629f31cf1fa20f96e2798386
Iaroslav <yslavcom.mon@gmail.com>
2024-11-24 07:07:22 +1100

    code for buildCharts

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-    const metadata = data.metadata
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
     const sampleMetadata = metadata.find(meta => meta.id == sample);
@@ -28,28 +28,54 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    const filteredSample = samples.filter(s => s.id == sample)[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
-
+    const otu_ids = filteredSample.otu_ids
+    const otu_labels = filteredSample.otu_labels;
+    const sample_values = filteredSample.sample_values;
+    
     // Build a Bubble Chart
-
+    const bubbleTrace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+      }
+    };
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", [bubbleTrace], bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    yticks = yticks.reverse();
+    
+    const barTrace = {
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+    };
 
+    const barLayout = {
+      title: "Top 10 OTUs",
+      margin: { t: 30, l: 150 }
+    };
 
     // Render the Bar Chart
+    Plotly.newPlot("bar", [barTrace], barLayout);
 
   });
 }
@@ -59,7 +85,7 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    const sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
 
